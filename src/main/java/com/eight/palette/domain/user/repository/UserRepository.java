ca12a8aaab9c98c5443a6e5ae385b6fe92909284
Ham-Seung-Wan <syham001@naver.com>
2024-07-11 14:24:29 +0900

    ✨ update : 리프레쉬 토큰 효율적으로 관리

--- src/main/java/com/eight/palette/domain/user/repository/UserRepository.java
+++ src/main/java/com/eight/palette/domain/user/repository/UserRepository.java
@@ -1,12 +1,26 @@
 package com.eight.palette.domain.user.repository;
 
 import com.eight.palette.domain.user.entity.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
     Optional<User> findByUsername(String username);
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true,
+            value = "update user " +
+                    "set refresh_token = :refreshToken " +
+                    "where id = :id")
+    void setTokenValue(@Param("id") Long id, @Param("refreshToken") String refreshToken);
+
 }
