8e3281306304486a6dae8cc2721def8cb0354a10
Ham-Seung-Wan <syham001@naver.com>
2024-07-11 14:27:03 +0900

    🩹 fix : 오타 수정

--- src/main/java/com/eight/palette/domain/user/entity/UserRoleEnum.java
+++ src/main/java/com/eight/palette/domain/user/entity/UserRoleEnum.java
@@ -2,7 +2,7 @@ package com.eight.palette.domain.user.entity;
 
 public enum UserRoleEnum {
     USER(Authority.USER),  // 사용자 권한
-    ADMIN(Authority.MANAGER);  // 관리자 권한
+    MANAGER(Authority.MANAGER);  // 관리자 권한
 
     private final String authority;
 
