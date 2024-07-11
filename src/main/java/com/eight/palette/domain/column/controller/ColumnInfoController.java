ae8d269dbc16b92dc6e57dcf4be0343fb8580806
Hyeonjin Kim <andrew75313@gmail.com>
2024-07-11 20:31:26 +0900

    [#1] ♻️ refactor : 컬럼 생성 시, 생성 컬럼 정보 응답으로 설정

--- src/main/java/com/eight/palette/domain/column/controller/ColumnInfoController.java
+++ src/main/java/com/eight/palette/domain/column/controller/ColumnInfoController.java
@@ -1,11 +1,14 @@
 package com.eight.palette.domain.column.controller;
 
 import com.eight.palette.domain.column.dto.ColumnInfoRequestDto;
+import com.eight.palette.domain.column.dto.ColumnInfoResponseDto;
 import com.eight.palette.domain.column.service.ColumnInfoService;
 import com.eight.palette.domain.user.entity.UserDetailsImpl;
+import com.eight.palette.global.dto.DataResponse;
 import com.eight.palette.global.dto.MessageResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -18,13 +21,15 @@ public class ColumnInfoController {
     private final ColumnInfoService columnInfoService;
 
     @PostMapping("/manager/boards/{boardId}/columns")
-    public ResponseEntity<MessageResponse> createColumn(@PathVariable(name = "boardId") Long boardId,
-                                                        @Valid @RequestBody ColumnInfoRequestDto columnInfoRequestDto,
-                                                        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public ResponseEntity<DataResponse<ColumnInfoResponseDto>> createColumn(@PathVariable(name = "boardId") Long boardId,
+                                                                            @Valid @RequestBody ColumnInfoRequestDto columnInfoRequestDto,
+                                                                            @AuthenticationPrincipal UserDetailsImpl userDetails) {
 
-        columnInfoService.createColumn(boardId, columnInfoRequestDto, userDetails.getUser());
+        ColumnInfoResponseDto responseDto = columnInfoService.createColumn(boardId, columnInfoRequestDto, userDetails.getUser());
 
-        return ResponseEntity.status(201).body(new MessageResponse(201, "컬럼 생성 성공 \uD83C\uDF89"));
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new DataResponse<>(HttpStatus.CREATED.value(),"컬럼 생성 성공 \uD83C\uDF89", responseDto ));
+
 
     }
 
