194fccd4728f6610434bf213bfd2d50f67c96a3a
Hyeonjin Kim <andrew75313@gmail.com>
2024-07-15 15:12:11 +0900

    [#29] 🎉 add : Redisson 동시성 제어를 위한 초기 설정
    -RedissonClient 을 위한 Config 생성
    -gradle에 Redisson 주입

---
+++ src/main/java/com/eight/palette/global/config/RedissonConfig.java
@@ -0,0 +1,33 @@
+package com.eight.palette.global.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissonConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private String redisPort;
+
+    private static final String REDISSON_PREFIX = "redis://";
+
+    @Bean
+    public RedissonClient redissonClient() {
+
+        Config config = new Config();
+
+        config.useSingleServer()
+                .setAddress(REDISSON_PREFIX + redisHost + ":" + redisPort);
+
+        return Redisson.create(config);
+
+    }
+
+}
